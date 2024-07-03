412dd4f45c3be693cd76d42fd341b3f42e1ca2e7
Roman Purtov <purtov143@gmail.com>
2024-07-04 03:52:32 +0600

    feat: create mock-data for entities

---
+++ src/entities/characters/charactersApi.mock.ts
@@ -0,0 +1,148 @@
+import ThreeD from '../assets/3d.jpg';
+import Abom from '../assets/abom.jpg';
+import Abraks from '../assets/abraks.jpg';
+import Abram from '../assets/abram.jpg';
+import Absor from '../assets/absor.jpg';
+import Adam from '../assets/adam.jpg';
+import Abys from '../assets/abys.jpg';
+import Comics1 from '../assets/comics1.jpg';
+import Comics2 from '../assets/comics2.jpg';
+import Comics3 from '../assets/comics3.jpg';
+import Comics4 from '../assets/comics4.jpg';
+import Comics5 from '../assets/comics5.jpg';
+
+
+export const charactersArray = [
+    {
+      id: '0',
+      Image: ThreeD,
+      title: 'Трехмерный человек',
+      description: 'Финал эпического путешествия сквозь время и пространство',
+    },
+    {
+      id: '1',
+      Image: Abom,
+      title: 'Мерзость',
+      description:
+        'В прошлом известный как Эмиль Блонски, советский шпион югославского происхождения, работавший на КГБ.',
+    },
+    {
+      id: '2',
+      Image: Abram,
+      title: 'Абрахам Ван Хельсинг',
+      description: 'Абрахам Ван Хельсинг был голландским профессором. ',
+    },
+    {
+      id: '3',
+      Image: Abraks,
+      title: 'Абраксас',
+      description: 'Абраксас - живое воплощение разрушения, в Мультивселенной Марвел. ',
+    },
+    {
+      id: '4',
+      Image: Adam,
+      title: 'Адам Уорлок',
+      description:
+        'Пропасть (Abyss) – мутант, обладатель уникальных (даже по меркам мутантов) способностей',
+    },
+    {
+      id: '5',
+      Image: Absor,
+      title: 'Поглотитель',
+      description:
+        'Крайне жестокий боксер, ставший преступником. Локи наделил его своей силой.',
+    },
+    {
+      id: '6',
+      Image: Abys,
+      title: 'Адам Уорлок',
+      description: 'Первоначально Адама Уорлока знали под именем - Его (Him).',
+    },
+    {
+      id: '7',
+      Image: Abys,
+      title: 'Адам Уорлок',
+      description: 'Первоначально Адама Уорлока знали под именем - Его (Him).',
+    },
+    {
+      id: '8',
+      Image: Abraks,
+      title: 'Абраксас',
+      description: 'Абраксас - живое воплощение разрушения, в Мультивселенной Марвел. ',
+    },
+    {
+      id: '9',
+      Image: Adam,
+      title: 'Адам Уорлок',
+      description:
+        'Пропасть (Abyss) – мутант, обладатель уникальных (даже по меркам мутантов) способностей',
+    },
+  ];
+
+
+  export const comicsArray = [
+    {
+      id: '0',
+      Image: Comics1,
+      title: 'Эра Альтрона. Книга 2',
+      description: 'Финал эпического путешествия сквозь время и пространство',
+    },
+    {
+      id: '1',
+      Image: Comics2,
+      title: 'Эра Альтрона. Книга 1',
+      description: 'Комикс, вдохновивший блокбастер "Мстители 2"!',
+    },
+    {
+      id: '2',
+      Image: Comics3,
+      title: 'Гамбит. Том 1. Рожденный вором...',
+      description: 'главный вор-мутант вселенной Marvel взваливает на себя слишком много, ',
+    },
+    {
+      id: '3',
+      Image: Comics4,
+      title: 'Дэдпул. Том 3',
+      description:
+        'Когда Циклоп объявил на весь мир, что Люди Икс создают на острове пристанище для мутантов,',
+    },
+    {
+      id: '4',
+      Image: Comics5,
+      title: 'Дэдпул. Том 4',
+      description:
+        'Немногие — герои, злодеи, наёмники и другие — способны затмить Удивительного Человека- ',
+    },
+    {
+      id: '5',
+      Image: Comics1,
+      title: 'Эра Альтрона. Книга 2',
+      description: 'Финал эпического путешествия сквозь время и пространство',
+    },
+    {
+      id: '6',
+      Image: Comics2,
+      title: 'Эра Альтрона. Книга 1',
+      description: 'Комикс, вдохновивший блокбастер "Мстители 2"!',
+    },
+    {
+      id: '7',
+      Image: Comics3,
+      title: 'Гамбит. Том 1. Рожденный вором...',
+      description: 'главный вор-мутант вселенной Marvel взваливает на себя слишком много, ',
+    },
+    {
+      id: '8',
+      Image: Comics4,
+      title: 'Дэдпул. Том 3',
+      description:
+        'Когда Циклоп объявил на весь мир, что Люди Икс создают на острове пристанище для мутантов,',
+    },
+    {
+      id: '9',
+      Image: Comics5,
+      title: 'Дэдпул. Том 4',
+      description:
+        'Немногие — герои, злодеи, наёмники и другие — способны затмить Удивительного Человека- ',
+    },
+  ];
