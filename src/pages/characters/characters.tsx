8eae017b76365795bffd88f53d62e84dc5d8bc32
Roman Purtov <purtov143@gmail.com>
2024-07-09 23:49:47 +0600

    fix: after review

--- src/pages/characters/characters.tsx
+++ src/pages/characters/characters.tsx
@@ -1,4 +1,4 @@
-import { ListItem } from '../../components/items/items';
+import { useNavigate } from 'react-router-dom';
 import { SearchButton } from '../../components/search-button/search-button';
 import { SearchInput } from '../../components/search-input/search-input';
 import { charactersArray } from '../../entities/characters/charactersApi.mock';
@@ -6,6 +6,12 @@ import { charactersArray } from '../../entities/characters/charactersApi.mock';
 import './characters.css';
 
 export const Caharacters = () => {
+  const navigate = useNavigate();
+
+  const handleClick = (id: string) => {
+      navigate(`/characters/${id}`);
+  };
+
   return (
     <div className="characters">
       <h2 className="characters__title">
@@ -17,13 +23,13 @@ export const Caharacters = () => {
       </div>
       <div className="characters__items">
         {charactersArray.map((c) => (
-          <ListItem
-            id={c.id}
-            Image={c.Image}
-            title={c.title}
-            entity="characters"
-            description={c.description}
-          />
+          <div onClick={() => handleClick(c.id)} className="list-item" key={c.id}>
+            <div className="list-item__image-container">
+              <img src={c.Image} alt={c.title} className="list-item__image" />
+            </div>
+            <h3 className="list-item__title">{c.title}</h3>
+            <p className="list-item__description">{c.description}</p>
+          </div>
         ))}
       </div>
     </div>
--- src/pages/comics/comics.tsx
+++ src/pages/comics/comics.tsx
@@ -1,9 +1,14 @@
-import { ListItem } from '../../components/items/items';
+import { useNavigate } from 'react-router-dom';
 import { SearchButton } from '../../components/search-button/search-button';
 import { SearchInput } from '../../components/search-input/search-input';
 import { comicsArray } from '../../entities/comics/comicsApi.mock';
 import './comics.css';
 export const Comics = () => {
+  const navigate = useNavigate();
+  const handleClick = (id: string) => {
+    navigate(`/comics/${id}`);
+};
+
   return (
     <div className="comics">
       <h2 className="comics__title">
@@ -13,15 +18,15 @@ export const Comics = () => {
         <SearchInput placeholder="Search comics by name" />
         <SearchButton />
       </div>
-      <div className="comics__items">
+      <div className="characters__items">
         {comicsArray.map((c) => (
-          <ListItem
-            id={c.id}
-            Image={c.Image}
-            entity="comics"
-            title={c.title}
-            description={c.description}
-          />
+          <div onClick={() => handleClick(c.id)} className="list-item" key={c.id}>
+            <div className="list-item__image-container">
+              <img src={c.Image} alt={c.title} className="list-item__image" />
+            </div>
+            <h3 className="list-item__title">{c.title}</h3>
+            <p className="list-item__description">{c.description}</p>
+          </div>
         ))}
       </div>
     </div>
